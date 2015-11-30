5222dbfe634834c0d7d66bc8c4baec6873c42728
Yurii Litvinov <yurii.litvinov@gmail.com>
2015-11-30 12:02:21 +0300

    Fixed bibliography, added Scopus part

--- litvinovPhDThesis/make.bat
+++ litvinovPhDThesis/make.bat
@@ -1,9 +1,11 @@
-# xelatex dissertation.tex
-# bibtex dissertation.aux
-# xelatex dissertation.tex
-# xelatex dissertation.tex
+xelatex dissertation.tex
+bibtex dissertation.aux
+xelatex dissertation.tex
+xelatex dissertation.tex
 
 xelatex synopsis.tex
-bibtex synopsis.aux
+bibtex synopsis.aux 
+bibtex vak.aux 
+bibtex scopus.aux 
 xelatex synopsis.tex
 xelatex synopsis.tex
